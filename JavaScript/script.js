9e2bb359b8c46bb9f5b93b9e97ae1bcb2633fae8
asus laptop <bhanusharma14581@gmail.com>
2025-07-11 23:40:59 +0530

    make it some better

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,9 +2,7 @@ const showPass = document.getElementById("show-and-hide");
 const passValue = document.getElementById("password");
 
 showPass.addEventListener("click", () => {
-  if (passValue.type == "password") {
-    passValue.type = "text";
-  } else {
-    passValue.type = "password";
-  }
+  const isHidden = passValue.type === "password";
+  passValue.type = isHidden ? "text" : "password";
+  showPass.textContent = isHidden ? "Hide Password" : "Show Password";
 });
